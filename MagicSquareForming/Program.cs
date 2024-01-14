89bd1e3beafae47a0001f6e94e806937bd0a56d3
Sahin YAKICI <shnykc@hotmail.com>
2024-01-14 13:08:04 +0300

    initial commit

---
+++ MagicSquareForming/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int formingMagicSquare(List<List<int>> s)
+    {
+        int count=s.Count;
+        
+        Console.WriteLine(count);
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<List<int>> s = new List<List<int>>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            s.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList());
+        }
+
+        int result = Result.formingMagicSquare(s);
+        Console.WriteLine(result);
+    }
+}
